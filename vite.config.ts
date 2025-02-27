b91f4ab404518b70029da5dbb7e4361c5dd3d253
José Gomes <j.da.silva8.gomes@gmail.com>
2025-02-27 18:24:08 +0000

    deploy url.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://da-silva-gomes.github.io/blackshield-code-challenge-vite/',
   plugins: [
     vue(),
     vueDevTools(),
